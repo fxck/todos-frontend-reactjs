d9de913e201f42356b465c0e96d646894af4728b
Ales Rechtorik <foxxcz@gmail.com>
2022-01-20 14:29:23 +0100

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,40 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 
 const queryClient = new QueryClient()
 
+const services = [
+  {
+    "hostname": "app",
+    "port": 80,
+    "url": "https://app-20109.app.zerops.io",
+    "repository": "https://github.com/fxck/zerops-recipe-base",
+    "type": "Nginx",
+    "containers": 1,
+    "category": "runtime"
+  },
+  {
+    "hostname": "api",
+    "port": 3000,
+    "url": "https://api-20109-5000.app.zerops.io",
+    "repository": "https://github.com/fxck/todos-backend-nestjs",
+    "type": "Node.js",
+    "containers": 3,
+    "category": "runtime"
+  },
+  {
+    "hostname": "db",
+    "port": 27017,
+    "type": "MongoDB",
+    "containers": 1,
+    "category": "database"
+  }
+];
+
+console.log('první log', services);
+
 const App: React.FC = () => (
   <>
+    <z-project-diagram services={JSON.stringify(services)}></z-project-diagram>
+
     <Header />
     <QueryClientProvider client={queryClient}>
       <TodoLayout />
---
+++ src/declarations.d.ts
@@ -0,0 +1,5 @@
+declare namespace JSX {
+  interface IntrinsicElements {
+    "z-project-diagram": any;
+  }
+}
